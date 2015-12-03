8594bd70fefa54521a78e4ba0865006383176a5b
Jente <jente@Scapy>
2015-12-03 13:46:52 +0100

    Changed variables and reply source

--- ping.py
+++ ping.py
@@ -3,10 +3,11 @@ from scapy.all import *
 
 TIMEOUT = 2
 conf.verb = 0
-for ip in range(140, 256):
-    packet = IP(dst="172.16.219." + str(ip), ttl=20)/ICMP()
-    reply = sr1(packet, timeout=TIMEOUT)
-    if not (reply is None):
-         print reply.dst, "is online"
-    else:
-         print "Timeout waiting for %s" % packet[IP].dst
+ip = "192.168.1.125"
+#for ip in range(140, 256):
+packet = IP(dst=ip, ttl=20)/ICMP()
+reply = sr1(packet, timeout=TIMEOUT)
+if not (reply is None):
+     print reply.src, "is online"
+else:
+     print "Timeout waiting for %s" % packet[IP].dst
--- tcp.py
+++ tcp.py
@@ -4,10 +4,11 @@ from scapy.all import *
 TIMEOUT = 2
 conf.verb = 0
 port=23
-for ip in range(140, 256):
-    packet = IP(dst="172.16.219." + str(ip), ttl=20)/TCP(dport=port)
-    reply = sr1(packet, timeout=TIMEOUT)
-    if not (reply is None):
-         print reply.dst, "is online"
-    else:
-         print "Timeout waiting for %s" % packet[IP].dst
+ip = "192.168.1.222"
+#for ip in range(140, 256):
+packet = IP(dst=ip, ttl=20)/TCP(dport=port)
+reply = sr1(packet, timeout=TIMEOUT)
+if not (reply is None):
+     print reply.src, "is online"
+else:
+     print "Timeout waiting for %s" % packet[IP].dst
