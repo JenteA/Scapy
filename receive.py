fd7fadae3c1ec2ffed1ed20d73a86e72dde200d9
Jente Adams <jente-29@hotmail.com>
2015-12-07 09:59:46 +0100

    added method

--- receive.py
+++ receive.py
@@ -4,6 +4,10 @@ from flask import render_template
 
 app = Flask(__name__)
 
+def run_script(id):
+    subprocess.call([select + ".py", ip])
+    background_scripts[id] = True
+
 @app.route('/')
 def my_form():
     return render_template("index.html")
@@ -13,7 +17,14 @@ def my_form_post():
 
     ip = request.form('ip')
     select = request.form.get('request')
-    return select +".py "+ip
+    
+@app.route('/generate')
+def generate():
+    id = str(uuid.uuid4())
+    background_scripts[id] = False
+    threading.Thread(target=lambda: run_script(id)).start()
+    return render_template('processing.html', id=id)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
