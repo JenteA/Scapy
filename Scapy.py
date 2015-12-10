2064f45f0244fde3a2cc6384a61c11306a04856a
Jente <jente.adams@gmail.com>
2015-12-10 17:48:17 +0100

    explain setcap en uitlezen post

--- Scapy.py
+++ Scapy.py
@@ -1,6 +1,12 @@
+from __future__ import print_function
+import sys
+from ip.py import ping
+from tcp.py import tcpping 
 from flask import Flask
 from flask import request
 from flask import render_template
+
+ip ='192.168.0.1'
 
 app = Flask(__name__)
 
@@ -18,6 +24,7 @@ def my_form_post():
     ip = request.form('ip')
     select = request.form.get('request')
     
+    
 @app.route('/generate')
 def generate():
     id = str(uuid.uuid4())
@@ -25,6 +32,9 @@ def generate():
     threading.Thread(target=lambda: run_script(id)).start()
     return render_template('processing.html', id=id)
 
+
+
+print(ip, file=sys.stderr)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
