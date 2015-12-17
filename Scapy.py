131175733b221ae52eaafa26880c7555619a9f5d
Jente <jente.adams@gmail.com>
2015-12-17 18:13:38 +0100

    Graag gedaan

--- Scapy.py
+++ Scapy.py
@@ -1,45 +1,43 @@
 from __future__ import print_function
+import subprocess
+import os
 import sys
 from ping import ping
 from tcp import tcpping 
 from flask import Flask
 from flask import request
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 ip ='192.168.0.1'
 
 app = Flask(__name__)
 
-@app.after_request
-def add_header(response):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-    response.headers['Cache-Control'] = 'public, max-age=0'
-    return response
-
 @app.route('/')
 def my_form():
+    #return redirect(url_for('index.html'))	
+    #return render_template(url_for("index.html"))
     return render_template("index.html")
 
-@app.route('/', methods=['POST'])
+@app.route('/Scriptje', methods=['POST'])
 def my_form_post():
-
-    ip = request.form('ip')
-    select = request.form.get('request')
-    print(ip);
+    ip = request.form['ip']
+    #select = str(request.form.get['request'])
+    print(ip)
+    subprocess.call("echo `pwd`>>succes", shell=True)
+    #subprocess.call("python_netraw ping.py", shell=True)
+    ping(ip)
+    #os.system("ping.py")
+    #background_scripts[id] = True
+    #execfile("ping.py")
+    return render_template("index.html", ip=ip)
     
-@app.route('/Scriptje')
+@app.route("/Scriptje")
 def run_script(id):
-    subprocess.call([select, ip])
+    subprocess.call("ping.py", shell=True)
+    #os.system("ping.py")
     background_scripts[id] = True
-
-
-
-
-print(ip, file=sys.stderr)
+    #execfile("ping.py")
 
 if __name__ == '__main__':
+    app.debug = True
     app.run(host='0.0.0.0')
--- ping.py
+++ ping.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 from scapy.all import *
 
-def ping():
+def ping(ip):
+
   TIMEOUT = 2
   conf.verb = 0
-  ip = "192.168.1.125"
+  #ip = "192.168.1.125"
   #for ip in range(140, 256):
   packet = IP(dst=ip, ttl=20)/ICMP()
   reply = sr1(packet, timeout=TIMEOUT)
@@ -13,3 +14,4 @@ def ping():
   else:
   	print "Timeout waiting for %s" % packet[IP].dst
   return;
+
