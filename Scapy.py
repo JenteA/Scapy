fc6433b30dc07a56c88b0eeb2e6d7406fc8201e5
Jente Adams <jente-29@hotmail.com>
2015-12-24 15:25:20 +0100

    added ARP + port choice of tcp

--- Scapy.py
+++ Scapy.py
@@ -3,7 +3,8 @@ import subprocess
 import os
 import sys
 from ping import ping
-from tcp import tcpping 
+from tcp import tcpping
+from arp import arp
 from flask import Flask
 from flask import request
 from flask import render_template, redirect, url_for
@@ -21,13 +22,16 @@ def my_form():
 def my_form_post():
     processText = ''
     ip = request.form['ip']
+    ip = request.form['port']
     choice = request.form['request']
     print(ip)
     subprocess.call("echo `pwd`>>succes", shell=True)
     if choice == 'ping':
        processText = ping(ip)
     if choice == 'tcp':
-	processText = tcpping(ip)
+	processText = tcpping(ip, port)
+    if choice == 'arp'
+       processText = arp(ip)
     else:
         print('nothing to do')
     return render_template("index.html", processText=processText)
--- arp.py
+++ arp.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python
 from scapy.all import *
-def tcpping(ip, port):
+def arp(ip):
   TIMEOUT = 2
   conf.verb = 0
-  #port=23
   #ip = "192.168.1.222"
   #for ip in range(140, 256):
-  packet = IP(dst=ip, ttl=20)/TCP(dport=port)
+  packet = Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip)
   reply = sr1(packet, timeout=TIMEOUT)
   if not (reply is None):
        return reply.src, "is online"
