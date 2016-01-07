972b1f14dcdab3e359a3c87bbbe129c720a50b98
Jente Adams <jente-29@hotmail.com>
2016-01-07 14:17:05 +0100

    added make list in arp

--- arp.py
+++ arp.py
@@ -4,10 +4,13 @@ from scapy.all import *
 def arp(ip, delay, nr):
   TIMEOUT = 2
   conf.verb = 0
+  rep=''
   for num in range(0, nr):
        packet = Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip)
        reply, unreply = srp(packet, timeout=TIMEOUT, iface="eth0")
        if len(reply) > 0:
-          return reply[0][0].getlayer(ARP).pdst, "is online"
+          rep[num] reply[0][0].getlayer(ARP).pdst, "is online"
        else:
-          return "Timeout waiting for %s" % packet[ARP].pdst
+          rep[num] "Timeout waiting for %s" % packet[ARP].pdst
+  sleep(delay) 
+  return rep
